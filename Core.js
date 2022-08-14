597e5d92aab1d62d97dd6256b8b9d784ca18d2fa
voiddd01 <96585585+voiddd01@users.noreply.github.com>
2022-08-14 18:50:27 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -3848,7 +3848,7 @@ case 'truth':
                  "do you play pubg, if you then send ur id number"
              ]
                            const mikutruthww = truth[Math.floor(Math.random() * truth.length)]
-                           buffer = await getBuffer(`https://wallpapercave.com/wp/wp10524609.jpg`)
+                           buffer = await getBuffer(`https://source.unsplash.com/720x600/?asthetic`)
                            Miku.sendMessage(from, { image: buffer, caption: '*You have chosen Truth*\n'+ mikutruthww }, {quoted:m})
                            break
 
